7bfbd2c707da715ced93d5c710db2f9d07049aee
agorodinskaya <agorodinskaya@gmail.com>
2018-07-02 03:30:02 +1000

    AJAX partly working

--- app/javascript/components/Webcam.js
+++ app/javascript/components/Webcam.js
@@ -1,38 +1,68 @@
 import React, {Component} from 'react';
 import Camera from 'react-webcam';
+import axios from 'axios';
+// import RNFS from 'react-native-fs'
 
-export default class Webcam extends Component {
+const K_ENROLL = 'https://api.kairos.com/enroll';
+
+export default class Register extends Component {
 
     constructor(props){
       super(props);
       this.state = {
-        screenshot: null,
-        tab: 0
+        photo: null,
+        status: false,
+        username: ''
       }
       this.buttonClick = this.buttonClick.bind(this);
     }
 
+
     buttonClick(){
-      const screenshot = this.webcam.getScreenshot();
-      this.setState({ screenshot });
+        const photo = this.webcam.getScreenshot();
+
+
+        this.setState({
+          photo,
+          status: true
+        });
+        console.log(photo, status);
+        axios.post(K_ENROLL, {
+          gallery_name: 'User',
+            image: photo,
+            subject_id: 'Test'
+        }, {        headers: {
+                    'Content-Type': 'application/json',
+                    app_id: '048de122',
+                    app_key: 'ff633fe82a3c8b6d0d85095c4aceac2f'
+                }
+            }).then((response) => {
+              console.log('response:', response, response.data  );
+                // this.setState({
+                //     status: false
+                // });
+        })
 
     };
+
+
 
     render() {
         return (
           <div>
-           <h1>react-webcam</h1>
+           <h1>Webcamera:</h1>
            <Camera
              audio={false}
+             // ?
              ref={node => this.webcam = node}
            />
-           
-             <h2>Screenshots</h2>
-             <div className='screenshots'>
+
+             <h2>Photo</h2>
+             <div className='photos'>
                <div className='controls'>
                  <button onClick={this.buttonClick}>Click</button>
                </div>
-               {this.state.screenshot ? <img src={this.state.screenshot} /> : null}
+               {this.state.photo ? <img src={this.state.photo} /> : null}
              </div>
            </div>
        );
